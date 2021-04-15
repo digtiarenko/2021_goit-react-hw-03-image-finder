c641c16aa9f857a7d23c8e19e26fb102f6516e30
Valentyn Digtiarenko <baggins999@gmail.com>
2021-04-15 19:13:08 +0300

    third commit

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -4,7 +4,7 @@ import styles from './Loader.module.css';
 const LoaderAnimation = () => {
   return (
     <div className={styles.Loader}>
-      <Loader type="Puff" color="#3f51b5" height={100} width={100} />
+      <Loader type="ThreeDots" color="#3f51b5" height={100} width={100} />
     </div>
   );
 };
